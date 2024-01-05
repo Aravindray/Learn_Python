dd8af9609b4b31130fcd21f7145d2c9caaa13c67
Aravindray <aravindselvasan@gmail.com>
2024-01-05 16:29:42 +0530

    working of hollow diamond pattern problem

--- JAN2024/Workout.py
+++ JAN2024/Workout.py
@@ -2,8 +2,9 @@ n = int(input('Enter no of rows: '))
 first_half = n // 2
 second_half = n - first_half
 inc = 1
+dec = n
 for i in range(1,first_half+1):
-    for j in range(n-i,0,-1):
+    for j in range(first_half-i,0,-1):
         print(' ',end=' ')
     for k in range(1,inc+1):
         if k == 1 or k == inc:
@@ -12,3 +13,5 @@ for i in range(1,first_half+1):
             print('#',end=' ')
     print()
     inc += 2
+for l in range(1,second_half+1):
+    for m in range
