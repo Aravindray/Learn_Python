b1a9fee2a08e44e3a6679eea1d17c02d2a66dc86
Aravindray <aravindselvasan@gmail.com>
2024-01-05 22:02:08 +0530

    workout file is for demo purpose

--- JAN2024/Workout.py
+++ JAN2024/Workout.py
@@ -1,17 +1,8 @@
-n = int(input('Enter no of rows: '))
-first_half = n // 2
-second_half = n - first_half
-inc = 1
-dec = n
-for i in range(1,first_half+1):
-    for j in range(first_half-i,0,-1):
-        print(' ',end=' ')
-    for k in range(1,inc+1):
-        if k == 1 or k == inc:
-            print('*',end=' ')
-        else:
-            print('#',end=' ')
-    print()
-    inc += 2
-for l in range(1,second_half+1):
-    for m in range
+def test1():
+    test1.a = 10
+    def test2():
+        test1.a = 8
+        print('Inside function test 2, test1.a =',test1.a)
+    test2()
+    print('Outside function test2, test1.a =',test1.a)
+test1()
