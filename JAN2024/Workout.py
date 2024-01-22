a6937212f8cc3be549c94ae82f9b3fd26c7b29f0
Aravindray <aravindselvasan@gmail.com>
2024-01-22 12:29:27 +0530

    commit it

--- JAN2024/Workout.py
+++ JAN2024/Workout.py
@@ -30,7 +30,11 @@ class Person:
         self.address = address
 
     def __str__(self):
-        return f'Name: {self.name}, DOB: {self.dob}, address: {self.address}'
+        return f'Name: {self.name},\nDOB: {self.dob},\naddress: {self.address}\n'
+    
+    def __del__(self):
+        Person.count -= 1
+        print('Deleted!')
 
 if __name__ == '__main__':
     p1 = Person('Aravind','16-05-1999','Earth')
@@ -38,3 +42,4 @@ if __name__ == '__main__':
     p2 = Person('Raj','16-05-1999','Earth')
     print(p2)
     print(Person.count)
+    print(p1.__str__())
