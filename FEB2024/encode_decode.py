47a2cc12e148b7b9f502858e537059f9d48b51ee
Aravindray <aravindselvasan@gmail.com>
2024-02-13 17:49:18 +0530

    created encode decode

--- FEB2024/encode_decode.py
+++ FEB2024/encode_decode.py
@@ -1,18 +1,104 @@
 # This module is to encode and decode a string with key
 import sys
+
+class Encode:
+    '''This class will encode the given string and returns a key'''
+    def __init__(self,message):
+        '''This is a class constructor'''
+        self.message = message
+        self.key()
+
+    def str_to_bin(self):
+        '''This method will convert sting to binary value'''
+        str_bin = ''
+        for x in self.message:
+            str_bin += format(ord(x),'b')
+        return str_bin
+    
+    def bin_to_invert(self):
+        '''This method will flip the binary value'''
+        bin_inv = ''
+        for x in self.str_to_bin():
+            if x == '1':
+                bin_inv += '0'
+            else:
+                bin_inv += '1'
+        return bin_inv
+    
+    def invert_to_morse(self):
+        '''This method will convert a digit to morse code
+        to make a mess I used ..... as 0 and _____ as 1'''
+        inv_morse = ''
+        for x in self.bin_to_invert():
+            if x == '0':
+                inv_morse += '.....'
+            else:
+                inv_morse += '_____'
+        return inv_morse
+    
+    def key(self):
+        '''This method will store the key in separate file'''
+        file = open('key.txt','w')
+        file.write(self.invert_to_morse())
+        file.close()
+    
+    def __str__(self):
+        '''This will print'''
+        return self.invert_to_morse()
+
+class Decode:
+    '''This class will decode the key and return the actual message'''
+    def __init__(self,key):
+        '''This is a class constructor'''
+        self.key = key
+
+    def morse_to_invert(self):
+        '''This method will convert the morse code and return 1st level of binary value'''
+        inc = 0
+        morse_inv = ''
+        for x in range(len(self.key)//5):
+            code = self.key[inc:inc+5]
+            if code == '.....':
+                morse_inv += '0'
+            else:
+                morse_inv += '1'
+            inc += 5
+        return morse_inv
+    
+    def invert_to_bin(self):
+        '''This method will flip the inverted binary to values to original binary'''
+        inv_bin = ''
+        for x in self.morse_to_invert():
+            if x == '1':
+                inv_bin += '0'
+            else:
+                inv_bin += '1'
+        return inv_bin
+    
+    def bin_to_str(self):
+        '''This method will convert the binary to string'''
+        inc = 0
+        bin_str = ''
+        for x in range(len(self.invert_to_bin())//7):
+            output = self.invert_to_bin()[inc:inc+7]
+            bin_str += chr(int(output,2))
+            inc += 7
+        return bin_str
+
+    def __str__(self):
+        '''This will print'''
+        return self.bin_to_str()
+
 encode_decode = input('Do you want to encode or decode? Type E for Encode or D for Decode: ')
 
 if encode_decode == 'E':
     message = input('Enter the message that you want to encode: ')
+    key = Encode(message)
+    print('Key successfully stored in key.txt file')
 elif encode_decode == 'D':
     key = input('Enter the key that you want to decode: ')
+    answer = Decode(key)
+    print(answer)
 else:
     print('Wrong input you should have either type E or D only')
     sys.exit()
-
-
-class Encode:
-    pass
-
-class Decode:
-    pass
--- workout.py
+++ workout.py
@@ -1,24 +1,56 @@
-# message = 'Iamopening#PassWords'
-# print(message)
-# bin_message = ''
-# for i in message:
-#     bin_message += str(ord(i))
-# print(bin_message)
-# dic_of_morse = {0 : '_____', 1 : '.____', 2 : '..___', 3 : '...__', 4 : '...._', 5 : '.....', 6 : '_....', 7 : '__...', 8 : '___..', 9 : '____.'}
-# key = ''
-# for x in bin_message:
-#     key += (dic_of_morse[int(x)])
+# string
+message = 'Aravind'
 
-# print(key)
+# string to binary
+str_bin = ''
+for x in message:
+    str_bin += format(ord(x),'b')
+print(str_bin)
 
+# bin to invert bin
+inv_bin = ''
+for x in str_bin:
+    if x == '1':
+        inv_bin += '0'
+    else:
+        inv_bin += '1'
+print(inv_bin)
 
-def string_to_binary(string):
-    return ' '.join(format(ord(x), 'b') for x in string)
-# binary to string
-def binary_to_string(binary):
-    return ''.join(chr(int(x, 2)) for x in binary.split())
-# test
-string = 'hello world'
-binary = string_to_binary(string)
-print(binary)
-print(binary_to_string(binary))
+# invert bin to morse
+morse = ''
+for x in inv_bin:
+    if x == '0':
+        morse += '.....'
+    else:
+        morse += '_____'
+print(morse)
+
+# morse to binary
+inc = 0
+mor_bin = ''
+for x in range(len(morse)//5):
+    code = morse[inc:inc+5]
+    if code == '.....':
+        mor_bin += '0'
+    else:
+        mor_bin += '1'
+    inc += 5
+print(mor_bin)
+
+# inv binary to bin
+inv_bin_bin = ''
+for x in mor_bin:
+    if x == '1':
+        inv_bin_bin += '0'
+    else:
+        inv_bin_bin += '1'
+print(inv_bin_bin)
+
+# bin to str
+bin_str = ''
+inc = 0
+for x in range(len(inv_bin_bin)//7):
+    output = inv_bin_bin[inc:inc+7]
+    bin_str += chr(int(output,2))
+    inc += 7
+print(bin_str)
