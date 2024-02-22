4d6759f15cde82b82e8bf0c94a038c8e387bc285
Aravindray <aravindselvasan@gmail.com>
2024-02-22 23:48:57 +0530

    added bubble sort

--- FEB2024/sorting.py
+++ FEB2024/sorting.py
@@ -1,15 +1,51 @@
 # Chapter 12 - List Manipulation
+import time
 
-# Selection Sorting
+class Sorting:
+    '''In this class I have implemented selection sort, bubble sort and Insertion sort technique'''
+    def __init__(self,lst):
+        '''This is class constructor with lst as list data type '''
+        self.lst = lst
 
-name = ['Vijaya', 'Sanvi', 'Ruby', 'Zafar', 'Maya', 'Anya']
-length_of_name = len(name)
+    def selection_sort(self):
+        '''Selection Sorting'''
+        length_of_lst = len(self.lst)
+        for i in range(length_of_lst):
+            mid_index = i
+            for j in range(i,length_of_lst-1):
+                if self.lst[mid_index] > self.lst[j+1]:
+                    mid_index = j+1
+            self.lst[i], self.lst[mid_index] = self.lst[mid_index], self.lst[i]
 
-for i in range(length_of_name):
-    mid_index = i
-    for j in range(i,length_of_name-1):
-        if name[mid_index] > name[j+1]:
-            mid_index = j+1
-    name[i], name[mid_index] = name[mid_index], name[i]
+    def bubble_sort(self):
+        '''Bubble Sorting from descending order'''
+        length_of_lst = len(self.lst)
+        for i in range(length_of_lst):
+            swap = False
+            for j in range(length_of_lst-1,i,-1):
+                if self.lst[j] < self.lst[j-1]:
+                    swap = True
+                    self.lst[j], self.lst[j-1] = self.lst[j-1], self.lst[j]
+            if swap == False:
+                break
 
-print(name)
+    def selection_sort(self):
+        '''Selection sorting'''
+        pass
+
+    def __str__(self):
+        '''This will return the lst'''
+        return f'{self.lst}'
+
+
+# names = ['Vijaya', 'Sanvi', 'Ruby', 'Zafar', 'Maya', 'Anya']
+# # numbers = [1001,1006,1002,1005,1004,1003]
+# s1 = Sorting(names)
+# print(s1)
+# s1.bubble_sort()
+# print(s1)
+
+new_names = ['Ruby', 'Sanvi', 'Maya', 'Vijaya', 'Anya', 'Zafar']
+s2 = Sorting(new_names)
+s2.bubble_sort()
+print(s2)
