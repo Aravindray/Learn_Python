ce3acb5e8d27ecf760ba4b626b78805c622d32c1
Aravindray <aravindselvasan@gmail.com>
2024-02-24 16:15:48 +0530

    working on case study

---
+++ FEB2024/case_study.py
@@ -0,0 +1,4 @@
+'''This module hold the answer of case study'''
+# import flie_object
+from students_twelve import Student
+from section import Section
---
+++ FEB2024/section.py
@@ -0,0 +1,5 @@
+'''This module hold section class and it's methods'''
+import pickle
+from students_twelve import Student
+
+class Section:
