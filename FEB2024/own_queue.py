af884885c1cd0a9cc1d371fcbf15f72421e22e88
Aravindray <aravindselvasan@gmail.com>
2024-03-01 12:39:37 +0530

    competed own queue

--- FEB2024/own_queue.py
+++ FEB2024/own_queue.py
@@ -17,12 +17,28 @@ class Queue:
             self (implicit parameter) - Object of type queue
             element - value to be inserted
         Return Value: None'''
+        if self.is_full():
+            print('Queue Overflow')
+        else:
+            if self.rear + 1 < self.qsize:
+                self.values[self.rear+1] = element
+                self.rear += 1
+            self.front = 0
 
     def dequeue(self):
         '''Objective: To remove an element from the front of queue
         Input Parameter:
             self (implicit parameter) - Object of type queue
         return value: front element of the queue if queue is not empty else None'''
+        if self.is_empty():
+            print('Queue underflow')
+            return None
+        temp = self.values[self.front]
+        for i in range(1, self.qsize):
+            self.values[i-1] = self.values[i]
+        self.rear -= 1
+        self.values[-1] = None
+        return temp
 
     def is_full(self):
         '''Objective: To determine whether the queue is full
@@ -38,7 +54,7 @@ class Queue:
         Return Value: True if the queue is empty else False'''
         return self.rear == -1
 
-    def front_value(self):
+    def front_element(self):
         '''Objective: To return element at the front of queue
         Input Parameter:
             self (implicit parameter) - Object of type queue
@@ -56,3 +72,10 @@ class Queue:
         if self.is_empty():
             return 0
         return self.rear - self.front + 1
+
+    def __str__(self):
+        '''This will print'''
+        string = str()
+        for elements in self.values:
+            string += str(elements) + ' '
+        return string
