302301b68a28da90c9b4577ffa80ae8a858c3f53
Aravindray <aravindselvasan@gmail.com>
2024-03-03 12:12:42 +0530

    learning linked list

---
+++ MAR2024/linked_list.py
@@ -0,0 +1,14 @@
+'''This module hold the actual linked list class'''
+from node import Node
+
+class LinkedList:
+    '''This is the collection of node called linked list'''
+    def __init__(self):
+        '''This class will create the head of the linked list as None'''
+        self.head = None
+
+    def insert_begin(self, value):
+        pass
+
+    def delete_begin(self, value):
+        pass
---
+++ MAR2024/node.py
@@ -0,0 +1,12 @@
+'''This is the primary module for linked list which hold node class'''
+
+class Node:
+    '''This class is part of the linked list which contain a object of node'''
+    def __init__(self,value):
+        '''This method contain value and next attribute'''
+        self.data = value
+        self.next = None
+
+    def __str__(self):
+        '''This method return string format of value and next'''
+        return f'{self.data} - [{self.next}]'
