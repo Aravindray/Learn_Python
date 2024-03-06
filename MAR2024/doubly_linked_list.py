56e8bdf21a3b0b413cb9e2b3e837aeb61938bc6a
Aravindray <aravindselvasan@gmail.com>
2024-03-06 10:21:22 +0530

    next level of linked list

---
+++ MAR2024/doubly_linked_list.py
@@ -0,0 +1,26 @@
+'''This module is for doubly linked list'''
+from node import DoubleNode
+
+class DoublyLinkedList:
+    '''This class hold the element with 2 way communication manner'''
+    def __init__(self):
+        '''This will constructed with empty list'''
+        self.head = None
+    
+    def insert_begin(self, value):
+        '''Insert the new element in the beginning of the list'''
+
+    def insert_end(self, value):
+        '''Insert the new element in the end of the list'''
+    
+    def delete_begin(self):
+        '''Delete the first element in the list'''
+
+    def delete_end(self):
+        '''Delete the last element in the list'''
+    
+    def delete_value(self, value):
+        '''Delete the given value from the linked list'''
+    
+    def __str__(self):
+        '''String representation of doubly linked list Previous:Data:Next'''
