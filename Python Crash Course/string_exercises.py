96bdf4cba2756c16e940468c5726ab2803533a80
Aravindray <aravindselvasan@gmail.com>
2024-09-07 21:33:02 +0530

    completing challenges

---
+++ Python Crash Course/string_exercises.py
@@ -0,0 +1,32 @@
+greetings = "Hello, Welcome!"
+print(greetings)
+
+bye = "Thanks for Coming!"
+print(bye)
+
+bye = "Thanks for Visiting!"
+print(bye)
+
+first_name = 'Eric'
+print(f"Hello {first_name}, would you like to learn some Python today?")
+
+name = "Aravindraj G"
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+famous_quote = 'Aravind once said, "Nothing".'
+print(famous_quote)
+
+famous_person = 'Aravind'
+print(f'{famous_person} once said, "Nothing"')
+
+full_name = ' Name: Aravindraj G\nSkills:\n\tPython\n\tJavaScript '
+print(full_name.rstrip())
+print(full_name.lstrip())
+print(full_name.strip())
+
+file_name = 'important.txt'
+file_name_2 = 'python_notes.txt'
+print(file_name.removesuffix('.txt'))
+print(file_name_2.removesuffix('.txt'))
