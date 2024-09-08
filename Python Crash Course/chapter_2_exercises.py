34b5c027fc79ba20377eda53842d151ce2cce7f8
Aravindray <aravindselvasan@gmail.com>
2024-09-08 17:05:41 +0530

    completed chapter 2 DIY

--- Python Crash Course/chapter_2_exercises.py
+++ Python Crash Course/chapter_2_exercises.py
@@ -1,35 +1,57 @@
 # Author: Aravind Date: Sun, 08/9 2024
 # Try it Yourself
 
+# 2-1
 greetings = "Hello, Welcome!"
 print(greetings)
 
+# 2-2
 bye = "Thanks for Coming!"
 print(bye)
 
 bye = "Thanks for Visiting!"
 print(bye)
 
+#2-3
 first_name = 'Eric'
 print(f"Hello {first_name}, would you like to learn some Python today?")
 
+#2-4
 name = "Aravindraj G"
 print(name.upper())
 print(name.lower())
 print(name.title())
 
+#2-5
 famous_quote = 'Aravind once said, "Nothing".'
 print(famous_quote)
 
+#2-6
 famous_person = 'Aravind'
 print(f'{famous_person} once said, "Nothing"')
 
+#2-7
 full_name = ' Name: Aravindraj G\nSkills:\n\tPython\n\tJavaScript '
 print(full_name.rstrip())
 print(full_name.lstrip())
 print(full_name.strip())
 
+#2-8
 file_name = 'important.txt'
 file_name_2 = 'python_notes.txt'
 print(file_name.removesuffix('.txt'))
 print(file_name_2.removesuffix('.txt'))
+
+#2-9
+print(4+4)
+print(100-92)
+print(4*2)
+print(120/15)
+print(120//15)
+
+#2-10
+favorite_number = 2
+fav_msg = f"Q: What is your favorite number?\nA: {favorite_number}"
+print(fav_msg)
+
+import this
