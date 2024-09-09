6348e2dbc3db066ba622bcad5ddba140b68ebd8e
Aravindray <aravindselvasan@gmail.com>
2024-09-09 20:33:47 +0530

    working on chapter 3 exercises

---
+++ Python Crash Course/chapter_3_exercises.py
@@ -0,0 +1,33 @@
+# Author: Aravind Date: Mon, 09/9 2024
+
+#3-1
+names = ['Aravind', 'Raj', 'Ray', 'Python']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+
+#3-2
+print(f"Hey {names[0].title()}, How are you?")
+print(f"Hey {names[1].title()}, How are you?")
+print(f"Hey {names[2].title()}, How are you?")
+print(f"Hey {names[3].title()}, How are you?")
+
+#3-3
+fav_transportations = ['Motorcycle', 'Car', 'Boat', 'Airplane', 'Bus', 'Train']
+print(f'I would like to own a Honda {fav_transportations[0].lower()}.')
+print(f'I would like to own a BMW {fav_transportations[1].lower()}.')
+print(f'I would like to own a Boston Whaler {fav_transportations[2].lower()}.')
+print(f'I would like to own a Boeing {fav_transportations[3].lower()}.')
+print(f'I would like to own a Volkswagen {fav_transportations[4].lower()}.')
+print(f'I would like to own a Lionel {fav_transportations[5].lower()}.')
+
+#3-4
+guests = ['Dad', 'Mom', 'Sister']
+print(f'Hey! Hello {guests[0]}, I would like to invite you for next week friday dinner to my house')
+print(f'Hey! Hello {guests[1]}, I would like to invite you for next week friday dinner to my house')
+print(f'Hey! Hello {guests[2]}, I would like to invite you for next week friday dinner to my house')
+
+#3-5
+
+print(f'Hello')
