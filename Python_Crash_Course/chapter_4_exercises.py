20a8b15f7728b2f042905aa9947b0d91fb94c079
Aravindray <aravindselvasan@gmail.com>
2024-09-14 14:53:40 +0530

    completed chapter 4 exercises

--- Python_Crash_Course/chapter_4_exercises.py
+++ Python_Crash_Course/chapter_4_exercises.py
@@ -1,14 +1,28 @@
 # Author: Aravind Date: Fri, 13/9 2024
 
-#4-1
+#4-1, 4-11
 pizzas = ['Margherita', 'New York-style', 'Neapolitan']
 for pizza in pizzas:
     # print(pizza)
     print(f'I like {pizza} pizza')
+
+print()
 print(f"I love to eat pizza everyday, The {pizzas[0]} pizza made my day.")
 print(f"And Wow, I love the thin crust and foldable pizza like {pizzas[1]}.")
 print(f"Who doesn't love mozzarella cheese I also love {pizzas[2]} pizza which full of mozzarella.")
 print('I really love pizza!')
+
+friend_pizzas = pizzas[:]
+pizzas.append('Chicago')
+friend_pizzas.append('Detroit')
+
+print('\nMy favorite pizzas are:')
+for pizza in pizzas:
+    print(f'\t{pizza}')
+
+print('\nMy friend\'s favorite pizzas are:')
+for pizza in friend_pizzas:
+    print(f'\t{pizza}')
 
 #4-2
 print()
@@ -46,13 +60,44 @@ print()
 multiple_threes = [x*3 for x in range(1, 11)]
 for number in multiple_threes:
     print(number)
+
+print()
 print(f"The first three items in the list are: {multiple_threes[:3]}")
 middle_index = (len(multiple_threes)//2) - 1
-print(f"Three items from the middle of the list are:"
-       "{multiple_threes[middle_index:middle_index+3]}")
+print(f"Three items from the middle of the list are: "
+      f"{multiple_threes[middle_index:middle_index+3]}")
+print(f"The last three items in the list are: {multiple_threes[-3:]}")
 
 #4-8, 4-9
 print()
 cubes = [x**3 for x in range(1,11)]
 for number in cubes:
     print(number)
+
+#4-12
+print()
+my_foods = ['pizza', 'falafel', 'carrot cake']
+print('My favorite foods are:')
+for food in my_foods:
+    print(f'\t{food}')
+
+print()
+friend_foods = my_foods[:]
+friend_foods.append('Ice cream')
+print('My friend\'s favorite foods are:')
+for food in friend_foods:
+    print(f'\t{food}')
+
+#4-13
+print()
+buffets = ('salad', 'bread rolls', 'pasta', 'roasted meats', 'dessert')
+print('Menu')
+for food in buffets:
+    print(food)
+# buffets[0] = 'veg salad'
+
+print()
+buffets = ('veg salad', 'PB&J', 'pasta', 'cheese potato', 'dessert')
+print("Revised Menu")
+for food in buffets:
+    print(food)
