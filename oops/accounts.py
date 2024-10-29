28ee250e573f5df5502314c795aecc8ab1c3f46a
Aravindray <aravindselvasan@gmail.com>
2024-10-29 19:37:35 +0530

    experimental

---
+++ oops/accounts.py
@@ -0,0 +1,8 @@
+class Account:
+    '''Represent bank user account'''
+    def __init__(self, username, opening_amount, password):
+        self.username = username
+        self.opening_amount = opening_amount
+        self.password = password
+    
+    
