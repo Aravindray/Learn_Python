b53531e771febf73c112eb2393c5ee8932f40442
Aravindray <aravindselvasan@gmail.com>
2024-10-31 13:25:06 +0530

    created template for pygame

--- learn_pygame/intro.py
+++ learn_pygame/intro.py
@@ -1,45 +1,45 @@
 '''Learning Pygame Basics'''
 
-# Import Packages
+# 1 - Import Packages
 import pygame
 from pygame.locals import *
 import sys
 
 # Learn to create a new windows using pygame
 
-# Define constants
+# 2 - Define constants
 BLACK = (0, 0, 0)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAMES_PER_SECOND = 30
 
-# Initialize the world
+# 3 - Initialize the world
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
-# Load assets: Image(s), sound(s), etc.
+# 4- Load assets: Image(s), sound(s), etc.
 
-# Initialize variables
+# 5- Initialize variables
 
-# Loop forever
+# 6- Loop forever
 while True:
 
-    # Check for handle events
+    # 7 - Check for handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    # Do any "per frame" actions
+    # 8 - Do any "per frame" actions
 
-    # Clear the window
+    # 9 - Clear the window
     window.fill(BLACK)
 
-    # Draw all window elements
+    # 10 -Draw all window elements
 
-    # Update the window
+    # 11 - Update the window
     pygame.display.update()
 
-    # Slow things down a bit
+    # 12 - Slow things down a bit
     clock.tick(FRAMES_PER_SECOND)
